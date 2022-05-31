e314c21c9d30ee04eae7fe68d10c37103c2b11d4
EggEggLiu <172204719@qq.com>
2022-05-31 23:24:51 +0800

    reactive basic

---
+++ src/reactivity/effect.ts
@@ -0,0 +1,46 @@
+class ReactiveEffect {
+    private _fn: any;
+
+    constructor(fn) {
+        this._fn = fn;
+    }
+    run() {
+        activeEffect = this;
+        const res = this._fn();
+        return res;
+    }
+
+}
+
+const targetMap = new WeakMap();
+export function track(target, key) {
+    let depsMap = targetMap.get(target);
+    if (!depsMap) {
+        depsMap = new Map();
+        targetMap.set(target, depsMap);
+    }
+
+    let deps = depsMap.get(key);
+    if (!deps) {
+        deps = new Set();
+        depsMap.set(key, deps);
+    }
+
+    deps.add(activeEffect);
+}
+
+export function trigger(target, key) {
+    const depsMap = targetMap.get(target);
+    if (!depsMap) return;
+    const deps = depsMap.get(key);
+    deps && deps.forEach(effect => effect.run());
+}
+
+let activeEffect;
+export function effect(fn) {
+    const _effect = new ReactiveEffect(fn);
+
+    _effect.run();
+
+    return _effect.run.bind(_effect);
+}
